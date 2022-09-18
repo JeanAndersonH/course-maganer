b3cb4872e40874b20604fc2885ccc18a3ea38bd1
JeanAndersonH <jeanhugo2016@gmail.com>
2022-09-17 22:47:50 -0300

    Ativando rotas ao inves de tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,39 @@
-
-
+import { Error404Component } from './error-404/error-404.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; /* importou pacote do formas module */
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/stat.component';
-
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 @NgModule({
   declarations: [                       /* todos os componentes devem ser declarados  */
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe,                    // pipe declarado, não um componente mas serve para alterar exibição e fazer alterações text
+    NavBarComponent,
+    Error404Component
     
   ],
   imports: [
     BrowserModule,
-    FormsModule                         /*importando modulo do angular */
+    FormsModule,                        /*importando modulo do angular */
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses',pathMatch:'full'      //raiz do projeto
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Component 
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
